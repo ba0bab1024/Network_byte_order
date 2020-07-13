5acddc95552daedc98eec361e19820e647ae3918
ba0bab1024 <8891ee1234@gmail.com>
2020-07-13 06:54:42 -0700

    Modify data type

--- my_ntohl.cpp
+++ my_ntohl.cpp
@@ -1,6 +1,7 @@
 #include "my_ntohl.h"
+#include <stdint.h>
 
-int my_ntohl(int n){
+uint32_t  my_ntohl(uint32_t  n){
     int n_1 =(n & 0xff000000) >> 24;
     int n_2 =(n & 0x00ff0000) >> 8;
     int n_3 =(n & 0x0000ff00) << 8;
